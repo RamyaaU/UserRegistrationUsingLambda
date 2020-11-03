bba7b3ae39afc6042d071842a5c0e413a20840b9
Ramya.U <ramyayadav2016@gmail.com>
2020-11-03 19:48:24 +0530

    [Ramya] Add . Added UC_1

--- UserRegistrationUsingLambda/Program.cs
+++ UserRegistrationUsingLambda/Program.cs
@@ -4,9 +4,24 @@ namespace UserRegistrationUsingLambda
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Reg using Lambda Expressions!");
+            Console.WriteLine("Enter First Name :");
+            string name = Console.ReadLine();
+            //reference fot regex class
+            RegeX regex = new RegeX();
+            //if regex is valid 
+            //it exexutes as valid firstname
+            //otherwise invalid firstname
+            if (regex.ValidFirstName(name))
+                Console.WriteLine("First Name is VALID !!");
+            else
+                Console.WriteLine("First Name is INVALID !!");
         }
     }
 }
---
+++ UserRegistrationUsingLambda/RegeX .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    public class RegeX
+    {
+        //pattern for valid name
+        public static string REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool ValidFirstName(string name)
+        {
+            return Regex.IsMatch(name, REGEX);
+        }
+    }
+}
