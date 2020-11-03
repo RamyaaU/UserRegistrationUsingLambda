03f2fa756bb49fe4b8869cf2156128dc186c1486
Ramya.U <ramyayadav2016@gmail.com>
2020-11-03 22:27:12 +0530

    [Ramya] Add . Added UC_12

---
+++ UserRegistrationUsingLambda/UserRegistrationException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistrationUsingLambda
+{
+    /// <summary>
+    /// Class implemented for custom exception
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class UserRegistrationException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_FIRSTNAME,
+            INVALID_LASTNAME,
+            INVALID_EMAIL,
+            INVALID_MOBILENUMBER,
+            INVALID_PASSWORD
+        }
+        private readonly ExceptionType type;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserRegistrationException"/> class.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="message">The message.</param>
+        public UserRegistrationException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
