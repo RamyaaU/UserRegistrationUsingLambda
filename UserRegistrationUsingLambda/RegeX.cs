a9798cf10fa4cfeaecb64b051dfa70f75eaac58f
Ramya.U <ramyayadav2016@gmail.com>
2020-11-03 19:54:40 +0530

    [Ramya] Add . Added UC_2

---
+++ UserRegistrationUsingLambda/RegeX.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    public class RegeX
+    {
+        //pattern for valid firstname
+        public static string FirstNameREGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        //pattern for valid lasttname
+        public static string LastNameREGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        /// <summary>
+        /// Valids the first name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public bool ValidFirstName(string name)
+        {
+            return Regex.IsMatch(name, FirstNameREGEX);
+        }
+
+        /// <summary>
+        /// Valids the last name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public bool ValidLastName(string name)
+        {
+            return Regex.IsMatch(name, LastNameREGEX);
+        }
+    }
+}
+   
