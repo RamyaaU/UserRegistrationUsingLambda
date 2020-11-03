681f4eddcf9cef0cc4166ef10f99490f9bf05c2e
Ramya.U <ramyayadav2016@gmail.com>
2020-11-03 20:00:36 +0530

    [Ramya] Add . Added UC_3

--- UserRegistrationUsingLambda/Program.cs
+++ UserRegistrationUsingLambda/Program.cs
@@ -7,6 +7,35 @@ namespace UserRegistrationUsingLambda
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Reg using Lambda Expressions!");
+            Console.WriteLine("Enter First Name :");
+            string name = Console.ReadLine();
+            //reference for regex class
+            RegeX regex = new RegeX();
+            //if regex is valid 
+            //it executes as valid firstname
+            //otherwise invalid firstname
+            if (regex.ValidFirstName(name))
+                Console.WriteLine("First Name is VALID !!");
+            else
+                Console.WriteLine("First Name is INVALID !!");
+            Console.WriteLine("Enter Last Name :");
+            //if regex is valid 
+            //it executes as valid lastname
+            //otherwise invalid lastname
+            string lastname = Console.ReadLine();
+            if (regex.ValidLastName(lastname))
+                Console.WriteLine("Last Name is VALID !!");
+            else
+                Console.WriteLine("Last Name is INVALID !!");
+            Console.WriteLine("Enter Email :");
+            string email = Console.ReadLine();
+            //if regex is valid 
+            //it executes as valid email
+            //otherwise invalid email
+            if (regex.ValidEmail(email))
+                Console.WriteLine("Email is VALID !!");
+            else
+                Console.WriteLine("Email is INVALID !!");
         }
     }
 }
---
+++ UserRegistrationUsingLambda/RegeX .cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    public class RegeX
+    {
+        //pattern for valid firstname
+        public static string FirstNameREGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        //pattern for valid lastname
+        public static string LastNameREGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        //pattern for valid email
+        public static string EmailREGEX = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
+        /// <summary>
+        /// Valids the first name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public bool ValidFirstName(string name)
+        {
+            return Regex.IsMatch(name, FirstNameREGEX);
+        }
+
+        /// <summary>
+        /// Valids the last name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public bool ValidLastName(string name)
+        {
+            return Regex.IsMatch(name, LastNameREGEX);
+        }
+        /// <summary>
+        /// Valids the email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns></returns>
+        public bool ValidEmail(string email)
+        {
+            return Regex.IsMatch(email, EmailREGEX); 
+        }
+    }
+}
